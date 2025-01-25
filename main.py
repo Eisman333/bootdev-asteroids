6112e2d63c14664fd4db4e3b4d1b276cc5e32995
Eisman333 <28490588+Eisman333@users.noreply.github.com>
2025-01-25 20:28:29 +0100

    add player instance

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 # delta time in seconds
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,7 @@ def main():
                 return
 
         screen.fill(pygame.Color("black"))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
